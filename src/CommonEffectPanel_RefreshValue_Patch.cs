b5f13ba9676ec064a4e235fcf9e62e0fba3e1633
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-03-18 23:48:29 -0400

    WIP

--- src/CommonEffectPanel_RefreshValue_Patch.cs
+++ src/CommonEffectPanel_RefreshValue_Patch.cs
@@ -3,6 +3,7 @@ using MGSC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
